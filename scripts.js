e1843f84e933a91e9eba088caa50f6e988aec672
Kayla Wood <kaylaewood@gmail.com>
2024-04-11 09:29:17 -0600

    Add JS file

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const aboutNav = document.querySelector('#about-btn');
+const visionNav = document.querySelector('#vision-btn');
+const applyNav = document.querySelector('#apply-btn');
+const contactNav = document.querySelector('#contact-btn');
+
