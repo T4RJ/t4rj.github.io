ff2c3f129e8e2e7b3ecf16d90b4d46eea3c7d946
Kayla Wood <kaylaewood@gmail.com>
2024-04-11 10:05:57 -0600

    Change tab colors on click

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 const navButtons = [
-  { name: 'about', selector: document.querySelector('#about-btn') },
-  { name: 'vision', selector: document.querySelector('#vision-btn') },
-  { name: 'apply', selector: document.querySelector('#apply-btn') },
-  { name: 'contact', selector: document.querySelector('#contact-btn') }
+  { name: 'about', selector: document.querySelector('#about-nav') },
+  { name: 'vision', selector: document.querySelector('#vision-nav') },
+  { name: 'apply', selector: document.querySelector('#apply-nav') },
+  { name: 'contact', selector: document.querySelector('#contact-nav') }
 ];
 
-const contentSections = [
+const contentPages = [
   { name: 'about', selector: document.querySelector('#about-section') },
   { name: 'vision', selector: document.querySelector('#vision-section') },
   { name: 'apply', selector: document.querySelector('#apply-section') },
@@ -19,12 +19,28 @@ navButtons.forEach(button => {
 });
 
 function changeDisplay(elementToShow) {
-  contentSections.forEach(section => {
+  showContent(elementToShow);
+  updateTabs(elementToShow);
+}
+
+function showContent(elementToShow) {
+  contentPages.forEach(section => {
     if (section.name === elementToShow) {
       section.selector.classList.remove('hidden');
     } else {
       section.selector.classList.add('hidden');
     }
   });
-  
+}
+
+function updateTabs(elementToShow) {
+  navButtons.forEach(button => {
+    if (button.name === elementToShow) {
+      button.selector.classList.add('active-nav');
+      button.selector.classList.remove('inactive-nav');
+    } else {
+      button.selector.classList.add('inactive-nav');
+      button.selector.classList.remove('active-nav');
+    }
+  });
 }
